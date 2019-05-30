f0a30e12a484024ff767ae8ed7b3f220027ec7d8
Khánh Trình <khanhtrinh2308@gmail.com>
2019-05-30 19:57:56 +0700

    bt2

--- Session2/Example1.php
+++ Session2/Example1.php
@@ -4,15 +4,28 @@
         echo  "<br/>";
     } */
 
-for ($i = 0; $i <= 100; $i++) {
+/*for ($i = 0; $i <= 100; $i++) {
     if ($i % 15 == 0) {
-            echo "So " . $i . " chia het cho 15 <br/>";
+        echo "So " . $i . " chia het cho 15 <br/>";
     } else if ($i % 3 == 0) {
         echo "So " . $i . " chia het cho 3 <br/>";
     } else if ($i % 5 == 0) {
         echo "So " . $i . " chia het cho 5 <br/>";
-    } 
-}
+    }
+}*/
 
-for ()
-?>
+//bt2 
+$sum = 0;
+$x = 2000;
+
+for ($i = 0; $i < 10; $i++){
+    if($x % 200 == 0){
+        $sum += 1;
+        if($sum % 2 == 0){
+            $sum +=1;
+        }
+    }   
+}
+echo $sum;
+
+//bt1
