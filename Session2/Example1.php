cfde0de389532f2808cbd43898f0f4572dd0abb3
Khánh Trình <khanhtrinh2308@gmail.com>
2019-05-30 19:20:13 +0700

    session 2 bai 1

---
+++ Session2/Example1.php
@@ -0,0 +1,18 @@
+<?php
+/*for ($i = 0; $i < 10; $i++) {
+        echo $i;
+        echo  "<br/>";
+    } */
+
+for ($i = 0; $i <= 100; $i++) {
+    if ($i % 15 == 0) {
+            echo "So " . $i . " chia het cho 15 <br/>";
+    } else if ($i % 3 == 0) {
+        echo "So " . $i . " chia het cho 3 <br/>";
+    } else if ($i % 5 == 0) {
+        echo "So " . $i . " chia het cho 5 <br/>";
+    } 
+}
+
+for ()
+?>
